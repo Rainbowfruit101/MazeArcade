eb93fb7431e099198bf43738e6f8ea79139aacda
Admin <rainbowfruit413@gmail.com>
2022-10-01 21:30:38 +0500

    Refactored views and coin generator.

--- Assets/Code/Views/CoinView.cs
+++ Assets/Code/Views/CoinView.cs
@@ -10,20 +10,20 @@ namespace Views
         private CoinModel _model;
         private CoinsController _coinsController;
 
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out PlayerView player))
+            {
+                _coinsController.OnCoinCollect(_model,this);
+            }
+        }
+
         public void Init(CoinModel model, CoinsController coinsController, Func<Vector2Int, Vector3> worldPositionProvider)
         {
             _model = model;
             _coinsController = coinsController;
             gameObject.name = $"Coin {_model.Position}";
             transform.position = worldPositionProvider.Invoke(_model.Position);
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.TryGetComponent(out PlayerController player))
-            {
-                _coinsController.OnCoinCollect(_model,this); 
-            }
         }
     }
 }
